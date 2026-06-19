a591c27549fbb769a0dbc643f9a05143a8f582fa
rohitwebdevo <rohit.career05@gmail.com>
2026-06-19 18:29:02 +0530

    Deploy folder adeded and script run

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%x5a8px_(*xl82l%-%q*-pkag&^ck&&a$gieww^ofiat&)w*^i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
